a8e1ce9bc6a6dbe8006a4eb637ba0afcd73e8d5e
Jarl Lyng <38857800+JarlLyng@users.noreply.github.com>
2025-02-26 00:05:29 +0100

    Initial Commit

---
+++ TrimrPix/TrimrPixApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrimrPixApp.swift
+//  TrimrPix
+//
+//  Created by Jarl Lyng on 26/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrimrPixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrimrPixTests/TrimrPixTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrimrPixTests.swift
+//  TrimrPixTests
+//
+//  Created by Jarl Lyng on 26/02/2025.
+//
+
+import Testing
+@testable import TrimrPix
+
+struct TrimrPixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
